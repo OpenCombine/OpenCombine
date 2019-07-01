989e5b1def6dee0e3d7229f3276e477e332b66cd
own2pwn <7850039+own2pwn@users.noreply.github.com>
2019-07-01 14:21:23 +0300

    Convenience sink method for use with Void Output type

--- Sources/OpenCombine/Subscribers/Subscribers.Sink.swift
+++ Sources/OpenCombine/Subscribers/Subscribers.Sink.swift
@@ -96,3 +96,27 @@ extension Publisher {
         return subscriber
     }
 }
+
+extension Publisher where Output == Void {
+    /// Attaches a subscriber with closure-based behavior.
+    ///
+    /// This method creates the subscriber and immediately requests an unlimited number
+    /// of values, prior to returning the subscriber.
+    /// - parameter receiveValue: The closure to execute on receipt of a value. If `nil`,
+    ///   the sink uses an empty closure.
+    /// - parameter receiveComplete: The closure to execute on completion. If `nil`,
+    ///   the sink uses an empty closure.
+    /// - Returns: A subscriber that performs the provided closures upon receiving values
+    ///   or completion.
+    public func sink(
+        receiveCompletion: ((Subscribers.Completion<Failure>) -> Void)? = nil,
+        receiveValue: @escaping (() -> Void)
+    ) -> Subscribers.Sink<Self> {
+        let subscriber = Subscribers.Sink<Self>(
+            receiveCompletion: receiveCompletion,
+            receiveValue: receiveValue
+        )
+        subscribe(subscriber)
+        return subscriber
+    }
+}
