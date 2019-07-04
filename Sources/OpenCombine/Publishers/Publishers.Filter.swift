d5dcb7876b28a432d5c3dd16e8c2f80646c080db
Joe Spadafora <spadafiva@gmail.com>
2019-07-03 22:47:43 -0400

    Adds filter and try filter implementations

---
+++ Sources/OpenCombine/Publishers/Publishers.Filter.swift
@@ -0,0 +1,181 @@
+//
+//  Publishers.Filter.swift
+//  
+//
+//  Created by Joseph Spadafora on 7/3/19.
+//
+
+import Foundation
+
+
+extension Publishers {
+    
+    /// A publisher that republishes all elements that match a provided closure.
+    public struct Filter<Upstream: Publisher>: Publisher {
+        
+        /// The kind of values published by this publisher.
+        public typealias Output = Upstream.Output
+        
+        /// The kind of errors this publisher might publish.
+        ///
+        /// Use `Never` if this `Publisher` does not publish errors.
+        public typealias Failure = Upstream.Failure
+        
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+        
+        /// A closure that indicates whether to republish an element.
+        public let isIncluded: (Upstream.Output) -> Bool
+        
+        public init(upstream: Upstream, isIncluded: @escaping (Output) -> Bool) {
+            self.upstream = upstream
+            self.isIncluded = isIncluded
+        }
+        
+        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
+        ///
+        /// - SeeAlso: `subscribe(_:)`
+        /// - Parameters:
+        ///     - subscriber: The subscriber to attach to this `Publisher`.
+        ///                   once attached it can begin to receive values.
+        public func receive<SubscriberType: Subscriber>(subscriber: SubscriberType)
+            where Upstream.Failure == SubscriberType.Failure,
+                  Upstream.Output == SubscriberType.Input
+        {
+            let filter = _Filter<Upstream, SubscriberType>(downstream: subscriber,
+                                                           isIncluded: isIncluded)
+            upstream.receive(subscriber: filter)
+        }
+    }
+    
+    /// A publisher that republishes all elements that match a provided error-throwing closure.
+    public struct TryFilter<Upstream> : Publisher where Upstream : Publisher {
+        
+        /// The kind of values published by this publisher.
+        public typealias Output = Upstream.Output
+        
+        /// The kind of errors this publisher might publish.
+        ///
+        /// Use `Never` if this `Publisher` does not publish errors.
+        public typealias Failure = Error
+        
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+        
+        /// A error-throwing closure that indicates whether to republish an element.
+        public let isIncluded: (Upstream.Output) throws -> Bool
+        
+        public init(upstream: Upstream,
+                    isIncluded: @escaping (Upstream.Output) throws -> Bool) {
+            self.upstream = upstream
+            self.isIncluded = isIncluded
+        }
+        
+        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
+        ///
+        /// - SeeAlso: `subscribe(_:)`
+        /// - Parameters:
+        ///     - subscriber: The subscriber to attach to this `Publisher`.
+        ///                   once attached it can begin to receive values.
+        public func receive<SubscriberType: Subscriber>(subscriber: SubscriberType)
+            where Upstream.Output == SubscriberType.Input,
+                  SubscriberType.Failure == Failure
+        {
+            let filter = _Filter<Upstream, SubscriberType>(downstream: subscriber,
+                                                           isIncluded: isIncluded)
+            upstream.receive(subscriber: filter)
+        }
+    }
+}
+
+extension Publisher {
+    
+    /// Republishes all elements that match a provided closure.
+    ///
+    /// - Parameter isIncluded: A closure that takes one element and returns a Boolean value indicating whether to republish the element.
+    /// - Returns: A publisher that republishes all elements that satisfy the closure.
+    public func filter(
+        _ isIncluded: @escaping (Self.Output) -> Bool
+    ) -> Publishers.Filter<Self> {
+        return Publishers.Filter(upstream: self, isIncluded: isIncluded)
+    }
+    
+    /// Republishes all elements that match a provided error-throwing closure.
+    ///
+    /// If the `isIncluded` closure throws an error, the publisher fails with that error.
+    ///
+    /// - Parameter isIncluded:  A closure that takes one element and returns a Boolean value indicating whether to republish the element.
+    /// - Returns:  A publisher that republishes all elements that satisfy the closure.
+    public func tryFilter(
+        _ isIncluded: @escaping (Self.Output) throws -> Bool
+    ) -> Publishers.TryFilter<Self> {
+        return Publishers.TryFilter(upstream: self, isIncluded: isIncluded)
+    }
+}
+
+
+private final class _Filter<Upstream: Publisher, Downstream: Subscriber>
+    : Subscriber,
+      CustomStringConvertible,
+      CustomReflectable,
+      Subscription
+    where Upstream.Output == Downstream.Input
+{
+    typealias Input = Upstream.Output
+    typealias Output = Downstream.Input
+    typealias Failure = Upstream.Failure
+    
+    private var _downstream: Downstream
+    private let _isIncluded: (Input) throws -> Bool
+    private var _upstreamSubscription: Subscription?
+    private var _demand: Subscribers.Demand = .none
+    
+    init(downstream: Downstream, isIncluded: @escaping (Input) throws -> Bool) {
+        self._isIncluded = isIncluded
+        self._downstream = downstream
+    }
+    
+    var description: String { return "Filter" }
+    
+    var customMirror: Mirror { return Mirror(self, children: EmptyCollection()) }
+    
+    func receive(subscription: Subscription) {
+        _upstreamSubscription = subscription
+        subscription.request(.unlimited)
+        _downstream.receive(subscription: self)
+    }
+    
+    func receive(_ input: Input) -> Subscribers.Demand {
+        do {
+            // input is filtered away, we just return the demand
+            if try _isIncluded(input) {
+                return _downstream.receive(input)
+            } else {
+                return _demand
+            }
+        } catch {
+            // We can force cast here because the regular filter never fails, so
+            _downstream.receive(completion: .failure(error as! Downstream.Failure))
+            cancel()
+            return .none
+        }
+    }
+    
+    func receive(completion: Subscribers.Completion<Failure>) {
+        switch completion {
+        case .finished:
+            _downstream.receive(completion: .finished)
+        case .failure(let error):
+            _downstream.receive(completion: .failure(error as! Downstream.Failure))
+        }
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        _demand = demand
+    }
+    
+    func cancel() {
+        _upstreamSubscription?.cancel()
+        _upstreamSubscription = nil
+    }
+}
---
+++ Tests/OpenCombineTests/PublisherTests/FilterTests.swift
@@ -0,0 +1,126 @@
+//
+//  FilterTests.swift
+//
+//
+//  Created by Joseph Spadafora on 6/25/19.
+//
+
+import XCTest
+
+#if OPENCOMBINE_COMPATIBILITY_TEST
+import Combine
+#else
+@testable
+import OpenCombine
+#endif
+
+@available(macOS 10.15, *)
+final class FilterTests: XCTestCase {
+    static let allTests = [
+        ("testFilterRemovesElements", testFilterRemovesElements),
+        ("testFilteringOtherFilters", testFilteringOtherFilters),
+        ("testTryFilterWorks", testTryFilterWorks),
+        ("testTryFilterCanFilterOtherFilter", testTryFilterCanFilterOtherFilter)
+    ]
+    
+    func testFilterRemovesElements() {
+        var results: [Int] = []
+        
+        let subscription = CustomSubscription()
+        let publisher = CustomPublisher(subscription: subscription)
+        _ = publisher.filter {
+            $0 % 2 == 0
+        }.sink {
+            results.append($0)
+        }
+        for i in 1...5 {
+            _ = publisher.send(i)
+        }
+        
+        XCTAssertEqual(results, [2, 4])
+    }
+    
+    func testFilteringOtherFilters() {
+        var results: [Int] = []
+        
+        let subscription = CustomSubscription()
+        let publisher = CustomPublisher(subscription: subscription)
+        _ = publisher.filter {
+            $0 % 3 == 0
+        }.filter {
+            $0 % 5 == 0
+        }.sink {
+            results.append($0)
+        }
+        for i in 1...15 {
+            _ = publisher.send(i)
+        }
+        
+        XCTAssertEqual(results, [15])
+    }
+    
+    func testTryFilterWorks() {
+        var results: [Int] = []
+        
+        let subscription = CustomSubscription()
+        let publisher = CustomPublisher(subscription: subscription)
+        _ = publisher.tryFilter {
+            try $0 % 2 == 0 && nonthrowingReturn($0)
+        }.sink {
+            results.append($0)
+        }
+        for i in 1...5 {
+            _ = publisher.send(i)
+        }
+        
+        XCTAssertEqual(results, [2, 4])
+    }
+    
+    func testTryFilterCanFilterOtherFilter() {
+        var results: [Int] = []
+        
+        let subscription = CustomSubscription()
+        let publisher = CustomPublisher(subscription: subscription)
+        _ = publisher.tryFilter {
+            $0 % 3 == 0
+        }.tryFilter {
+            try nonthrowingReturn($0)
+        }.sink {
+            results.append($0)
+        }
+        for i in 1...9 {
+            _ = publisher.send(i)
+        }
+        
+        XCTAssertEqual(results, [3, 6, 9])
+    }
+    
+    func testTryFilterCompletesWithErrorWhenThrown() {
+        let subscription = CustomSubscription()
+        let publisher = CustomPublisher(subscription: subscription)
+        var error: TestingError?
+        _ = publisher.tryFilter {
+            try failOnFive(value: $0)
+        }.sink(receiveCompletion: { completion in
+            if case let .failure(completionError) = completion {
+                error = completionError as? TestingError
+            }
+        }, receiveValue: { _ in })
+        for i in 1...5 {
+            _ = publisher.send(i)
+        }
+        XCTAssertEqual(subscription.history, [.requested(.unlimited), .cancelled])
+        XCTAssertEqual(error, TestingError.oops)
+    }
+}
+
+private func nonthrowingReturn(_ value: Int) throws -> Bool {
+    return true
+}
+
+private func failOnFive(value: Int) throws -> Bool {
+    if value == 5 {
+        throw TestingError.oops
+    }
+    return true
+}
