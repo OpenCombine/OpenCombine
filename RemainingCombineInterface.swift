df8975103a0a560dccbde99f85a2e03ae24795f1
Joe Spadafora <spadafiva@gmail.com>
2019-07-04 11:28:15 -0400

    Adds MapError implementation

--- RemainingCombineInterface.swift
+++ RemainingCombineInterface.swift
@@ -2033,45 +2033,6 @@ extension Publisher {
 
 extension Publishers {
 
-    /// A publisher that converts any failure from the upstream publisher into a new error.
-    public struct MapError<Upstream, Failure> : Publisher where Upstream : Publisher, Failure : Error {
-
-        /// The kind of values published by this publisher.
-        public typealias Output = Upstream.Output
-
-        /// The publisher from which this publisher receives elements.
-        public let upstream: Upstream
-
-        /// The closure that converts the upstream failure into a new error.
-        public let transform: (Upstream.Failure) -> Failure
-
-        public init(upstream: Upstream, transform: @escaping (Upstream.Failure) -> Failure)
-
-        public init(upstream: Upstream, _ map: @escaping (Upstream.Failure) -> Failure)
-
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where Failure == S.Failure, S : Subscriber, Upstream.Output == S.Input
-    }
-}
-
-extension Publisher {
-
-    /// Converts any failure from the upstream publisher into a new error.
-    ///
-    /// Until the upstream publisher finishes normally or fails with an error, the returned publisher republishes all the elements it receives.
-    ///
-    /// - Parameter transform: A closure that takes the upstream failure as a parameter and returns a new error for the publisher to terminate with.
-    /// - Returns: A publisher that replaces any upstream failure with a new error produced by the `transform` closure.
-    public func mapError<E>(_ transform: @escaping (Self.Failure) -> E) -> Publishers.MapError<Self, E> where E : Error
-}
-
-extension Publishers {
-
     /// A publisher that publishes either the most-recent or first element published by the upstream publisher in a specified time interval.
     public struct Throttle<Upstream, Context> : Publisher where Upstream : Publisher, Context : Scheduler {
 
