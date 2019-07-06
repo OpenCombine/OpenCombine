7da14a07226b20c0ee2ea8b955396c1f8b473988
Joe Spadafora <spadafiva@gmail.com>
2019-07-06 00:13:30 -0400

    Remove extra comment

--- Tests/OpenCombineTests/SubscribersTests/CompletionTests.swift
+++ Tests/OpenCombineTests/SubscribersTests/CompletionTests.swift
@@ -28,8 +28,6 @@ final class CompletionTests: XCTestCase {
         let encoder = JSONEncoder()
         if #available(iOS 11.0, *) {
             encoder.outputFormatting = .sortedKeys
-        } else {
-            // Fallback on earlier versions
         }
         return encoder
     }()
