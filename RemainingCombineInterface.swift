2f2e16ee1f5f73af8980efb8541ec61a13797d94
Joe Spadafora <spadafiva@gmail.com>
2019-07-09 22:44:41 -0400

    Implement Last, LastWhere, TryLastWhere. Use ReduceProducer.

--- RemainingCombineInterface.swift
+++ RemainingCombineInterface.swift
@@ -1413,80 +1413,6 @@ extension Publisher {
 
 extension Publishers {
 
-    /// A publisher that only publishes the last element of a stream that satisfies a predicate closure, once the stream finishes.
-    public struct LastWhere<Upstream> : Publisher where Upstream : Publisher {
-
-        /// The kind of values published by this publisher.
-        public typealias Output = Upstream.Output
-
-        /// The kind of errors this publisher might publish.
-        ///
-        /// Use `Never` if this `Publisher` does not publish errors.
-        public typealias Failure = Upstream.Failure
-
-        /// The publisher from which this publisher receives elements.
-        public let upstream: Upstream
-
-        /// The closure that determines whether to publish an element.
-        public let predicate: (Upstream.Output) -> Bool
-
-        public init(upstream: Upstream, predicate: @escaping (Publishers.LastWhere<Upstream>.Output) -> Bool)
-
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where S : Subscriber, Upstream.Failure == S.Failure, Upstream.Output == S.Input
-    }
-
-    /// A publisher that only publishes the last element of a stream that satisfies a error-throwing predicate closure, once the stream finishes.
-    public struct TryLastWhere<Upstream> : Publisher where Upstream : Publisher {
-
-        /// The kind of values published by this publisher.
-        public typealias Output = Upstream.Output
-
-        /// The kind of errors this publisher might publish.
-        ///
-        /// Use `Never` if this `Publisher` does not publish errors.
-        public typealias Failure = Error
-
-        /// The publisher from which this publisher receives elements.
-        public let upstream: Upstream
-
-        /// The error-throwing closure that determines whether to publish an element.
-        public let predicate: (Upstream.Output) throws -> Bool
-
-        public init(upstream: Upstream, predicate: @escaping (Publishers.TryLastWhere<Upstream>.Output) throws -> Bool)
-
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where S : Subscriber, Upstream.Output == S.Input, S.Failure == Publishers.TryLastWhere<Upstream>.Failure
-    }
-}
-
-extension Publisher {
-
-    /// Only publishes the last element of a stream that satisfies a predicate closure, after the stream finishes.
-    /// - Parameter predicate: A closure that takes an element as its parameter and returns a Boolean value indicating whether to publish the element.
-    /// - Returns: A publisher that only publishes the last element satisfying the given predicate.
-    public func last(where predicate: @escaping (Self.Output) -> Bool) -> Publishers.LastWhere<Self>
-
-    /// Only publishes the last element of a stream that satisfies a error-throwing predicate closure, after the stream finishes.
-    ///
-    /// If the predicate closure throws, the publisher fails with the thrown error.
-    /// - Parameter predicate: A closure that takes an element as its parameter and returns a Boolean value indicating whether to publish the element.
-    /// - Returns: A publisher that only publishes the last element satisfying the given predicate.
-    public func tryLast(where predicate: @escaping (Self.Output) throws -> Bool) -> Publishers.TryLastWhere<Self>
-}
-
-extension Publishers {
-
     /// A publisher that “flattens” nested publishers.
     ///
     /// Given a publisher that publishes Publishers, the `SwitchToLatest` publisher produces a sequence of events from only the most recent one.
@@ -2053,41 +1979,6 @@ extension Publisher {
     ///   - options: Scheduler options that customize this publisher’s delivery of elements.
     /// - Returns: A publisher that publishes events only after a specified time elapses.
     public func debounce<S>(for dueTime: S.SchedulerTimeType.Stride, scheduler: S, options: S.SchedulerOptions? = nil) -> Publishers.Debounce<Self, S> where S : Scheduler
-}
-
-extension Publishers {
-
-    /// A publisher that only publishes the last element of a stream, after the stream finishes.
-    public struct Last<Upstream> : Publisher where Upstream : Publisher {
-
-        /// The kind of values published by this publisher.
-        public typealias Output = Upstream.Output
-
-        /// The kind of errors this publisher might publish.
-        ///
-        /// Use `Never` if this `Publisher` does not publish errors.
-        public typealias Failure = Upstream.Failure
-
-        /// The publisher from which this publisher receives elements.
-        public let upstream: Upstream
-
-        public init(upstream: Upstream)
-
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where S : Subscriber, Upstream.Failure == S.Failure, Upstream.Output == S.Input
-    }
-}
-
-extension Publisher {
-
-    /// Only publishes the last element of a stream, after the stream finishes.
-    /// - Returns: A publisher that only publishes the last element of a stream.
-    public func last() -> Publishers.Last<Self>
 }
 
 extension Publishers {
---
+++ Tests/OpenCombineTests/PublisherTests/LastTests.swift
@@ -0,0 +1,367 @@
+//
+//  LastTests.swift
+//  
+//
+//  Created by Joseph Spadafora on 7/9/19.
+//
+
+import XCTest
+
+#if OPENCOMBINE_COMPATIBILITY_TEST
+import Combine
+#else
+import OpenCombine
+#endif
+
+@available(macOS 10.15, iOS 13.0, *)
+final class LastTests: XCTestCase {
+
+    // MARK: - Last
+
+    func testLastFinishesAndReturnsLastItem() {
+        let helper = OperatorTestHelper(publisherType: CustomPublisher.self,
+                                        initialDemand: .max(3),
+                                        receiveValueDemand: .max(1),
+                                        createSut: { $0.last() })
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("Last")])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(25), .none)
+        XCTAssertEqual(helper.publisher.send(42), .none)
+        XCTAssertEqual(helper.publisher.send(10), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("Last")])
+
+        helper.publisher.send(completion: .finished)
+        XCTAssertEqual(helper.tracking.history, [.subscription("Last"),
+                                                 .value(10),
+                                                 .completion(.finished)])
+
+        XCTAssertEqual(helper.publisher.send(73), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("Last"),
+                                                 .value(10),
+                                                 .completion(.finished)])
+    }
+
+    func testLastFinishesWithError() {
+        ReduceTests.testUpstreamFinishesWithError(expectedSubscription: "Last") {
+            $0.last()
+        }
+    }
+
+    func testLastFinishesImmediately() {
+        ReduceTests.testUpstreamFinishesImmediately(expectedSubscription: "Last",
+                                                    expectedResult: nil) {
+            $0.last()
+        }
+    }
+
+    func testLastRequestsUnlimitedThenSendsSubscription() {
+        ReduceTests.testRequestsUnlimitedThenSendsSubscription { $0.last() }
+    }
+
+    func testLastReceiveSubscriptionTwice() throws {
+        try ReduceTests.testReceiveSubscriptionTwice(
+            expectedSubscription: "Last",
+            expectedResult: .normalCompletion(0),
+            { $0.last() }
+        )
+    }
+
+    func testLastLifecycle() throws {
+        try testLifecycle(sendValue: 31,
+                          cancellingSubscriptionReleasesSubscriber: false,
+                          { $0.last() })
+    }
+
+    func testLastCancelAlreadyCancelled() throws {
+        try ReduceTests.testCancelAlreadyCancelled { $0.last() }
+    }
+
+    func testLastReflection() throws {
+        try testReflection(parentInput: Int.self,
+                           parentFailure: Error.self,
+                           description: "Last",
+                           customMirror: reduceLikeOperatorMirror(),
+                           playgroundDescription: "Last",
+                           { $0.last() })
+    }
+
+    // MARK: - LastWhere
+
+    func testLastWhereDemand() throws {
+
+        var firedCounter = 0
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: nil,
+            receiveValueDemand: .none,
+            createSut: {
+                $0.last {
+                    firedCounter += 1
+                    return $0 > 1
+                }
+            }
+        )
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(3), .none)
+        XCTAssertEqual(helper.publisher.send(2), .none)
+        XCTAssertEqual(helper.publisher.send(1), .none)
+        XCTAssertEqual(helper.publisher.send(0), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+        XCTAssertEqual(firedCounter, 4)
+
+        try XCTUnwrap(helper.downstreamSubscription).request(.unlimited)
+        try XCTUnwrap(helper.downstreamSubscription).request(.max(1))
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+
+        helper.publisher.send(completion: .finished)
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere"),
+                                                 .value(2),
+                                                 .completion(.finished)])
+
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+    }
+
+    func testLastWhereFinishesAndReturnsLastMatchingItem() {
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: .max(5),
+            receiveValueDemand: .max(1),
+            createSut: { $0.last(where: { $0 < 3 }) }
+        )
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(1), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+
+        XCTAssertEqual(helper.publisher.send(2), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+
+        XCTAssertEqual(helper.publisher.send(3), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere")])
+
+        helper.publisher.send(completion: .finished)
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere"),
+                                                 .value(2),
+                                                 .completion(.finished)])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(4), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("LastWhere"),
+                                                 .value(2),
+                                                 .completion(.finished)])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+    }
+
+    func testLastWhereFinishesWithError() {
+        ReduceTests.testUpstreamFinishesWithError(expectedSubscription: "LastWhere") {
+            $0.last(where: { $0 > 2 })
+        }
+    }
+
+    func testLastWhereFinishesImmediately() {
+        ReduceTests.testUpstreamFinishesImmediately(expectedSubscription: "LastWhere",
+                                                    expectedResult: nil) {
+            $0.last(where: { $0 > 2 })
+        }
+    }
+
+    func testLastWhereRequestsUnlimitedThenSendsSubscription() {
+        ReduceTests.testRequestsUnlimitedThenSendsSubscription {
+            $0.last(where: { $0 > 0 })
+        }
+    }
+
+    func testLastWhereReceiveSubscriptionTwice() throws {
+        try ReduceTests.testReceiveSubscriptionTwice(
+            expectedSubscription: "LastWhere",
+            expectedResult: .normalCompletion(nil),
+            { $0.last(where: { _ in false }) }
+        )
+
+        try ReduceTests.testReceiveSubscriptionTwice(
+            expectedSubscription: "LastWhere",
+            expectedResult: .normalCompletion(0),
+            { $0.last(where: { _ in true }) }
+        )
+    }
+
+    func testLastWhereLifecycle() throws {
+        try testLifecycle(sendValue: 31,
+                          cancellingSubscriptionReleasesSubscriber: false,
+                          { $0.last { $0 > 1 } })
+    }
+
+    func testLastWhereCancelAlreadyCancelled() throws {
+        try ReduceTests.testCancelAlreadyCancelled { $0.last(where: { $0 > 2 }) }
+    }
+
+    func testLastWhereReflection() throws {
+        try testReflection(parentInput: Int.self,
+                           parentFailure: Error.self,
+                           description: "LastWhere",
+                           customMirror: reduceLikeOperatorMirror(),
+                           playgroundDescription: "LastWhere",
+                           { $0.last(where: { $0 > 2 }) })
+    }
+
+    // MARK: - TryLastWhere
+
+    func testTryLastWhereDemand() throws {
+
+        var firedCounter = 0
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: nil,
+            receiveValueDemand: .none,
+            createSut: {
+                $0.tryLast {
+                    firedCounter += 1
+                    return $0 > 1
+                }
+            }
+        )
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(3), .none)
+        XCTAssertEqual(helper.publisher.send(2), .none)
+        XCTAssertEqual(helper.publisher.send(1), .none)
+        XCTAssertEqual(helper.publisher.send(0), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+        XCTAssertEqual(firedCounter, 4)
+
+        try XCTUnwrap(helper.downstreamSubscription).request(.unlimited)
+        try XCTUnwrap(helper.downstreamSubscription).request(.max(1))
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+
+        helper.publisher.send(completion: .finished)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere"),
+                                                 .value(2),
+                                                 .completion(.finished)])
+
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+    }
+
+    func testTryLastWhereFinishesAndReturnsLastMatchingItem() {
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: .max(5),
+            receiveValueDemand: .max(1),
+            createSut: { $0.tryLast(where: { $0 < 3 }) }
+        )
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(1), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+
+        XCTAssertEqual(helper.publisher.send(2), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+
+        XCTAssertEqual(helper.publisher.send(3), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere")])
+
+        helper.publisher.send(completion: .finished)
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere"),
+                                                 .value(2),
+                                                 .completion(.finished)])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+
+        XCTAssertEqual(helper.publisher.send(4), .none)
+        XCTAssertEqual(helper.tracking.history, [.subscription("TryLastWhere"),
+                                                 .value(2),
+                                                 .completion(.finished)])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
+    }
+
+    func testTryLastWhereFinishesWithError() {
+        ReduceTests.testUpstreamFinishesWithError(expectedSubscription: "TryLastWhere") {
+            $0.tryLast(where: { $0 > 2 })
+        }
+    }
+
+    func testTryLastWhereFinishesImmediately() {
+        ReduceTests
+            .testUpstreamFinishesImmediately(expectedSubscription: "TryLastWhere",
+                                             expectedResult: nil) {
+                $0.tryLast(where: { $0 > 2 })
+            }
+    }
+
+    func testTryLastWhereRequestsUnlimitedThenSendsSubscription() {
+        ReduceTests.testRequestsUnlimitedThenSendsSubscription {
+            $0.tryLast(where: { $0 > 0 })
+        }
+    }
+
+    func testTryLastWhereFinishesWhenErrorThrown() throws {
+
+        func predicate(_ input: Int) throws -> Bool {
+            if input == 3 {
+                throw TestingError.oops
+            }
+            return input < 3
+        }
+
+        try ReduceTests.testFailureBecauseOfThrow(expectedSubscription: "TryLastWhere",
+                                                  expectedFailure: TestingError.oops,
+                                                  { $0.tryLast(where: predicate) })
+    }
+
+    func testTryLastWhereReceiveSubscriptionTwice() throws {
+        try ReduceTests.testReceiveSubscriptionTwice(
+            expectedSubscription: "TryLastWhere",
+            expectedResult: .normalCompletion(nil),
+            { $0.tryLast(where: { _ in false }) }
+        )
+
+        try ReduceTests.testReceiveSubscriptionTwice(
+            expectedSubscription: "TryLastWhere",
+            expectedResult: .normalCompletion(0),
+            { $0.tryLast(where: { _ in true }) }
+        )
+
+        try ReduceTests.testReceiveSubscriptionTwice(
+            expectedSubscription: "TryLastWhere",
+            expectedResult: .failure(TestingError.oops),
+            { $0.tryLast(where: { _ in throw TestingError.oops }) }
+        )
+    }
+
+    func testTryLastWhereLifecycle() throws {
+        try testLifecycle(sendValue: 31,
+                          cancellingSubscriptionReleasesSubscriber: false,
+                          { $0.tryLast { $0 > 1 } })
+    }
+
+    func testTryLastCancelAlreadyCancelled() throws {
+        try ReduceTests.testCancelAlreadyCancelled { $0.tryLast(where: { $0 > 2 }) }
+    }
+
+    func testTryLastWhereReflection() throws {
+        try testReflection(parentInput: Int.self,
+                           parentFailure: Error.self,
+                           description: "TryLastWhere",
+                           customMirror: reduceLikeOperatorMirror(),
+                           playgroundDescription: "TryLastWhere",
+                           { $0.tryLast(where: { $0 > 2 }) })
+    }
+}
