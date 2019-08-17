c2b9cb68a570b459075f3486f0d787ed66b34989
Eric Patey <eric@patey.com>
2019-08-17 08:43:04 -0400

    Implement FlatMap

--- RemainingCombineInterface.swift
+++ RemainingCombineInterface.swift
@@ -2735,51 +2735,6 @@ extension Publisher {
 
 extension Publishers {
 
-    public struct FlatMap<NewPublisher, Upstream> : Publisher where NewPublisher : Publisher, Upstream : Publisher, NewPublisher.Failure == Upstream.Failure {
-
-        /// The kind of values published by this publisher.
-        public typealias Output = NewPublisher.Output
-
-        /// The kind of errors this publisher might publish.
-        ///
-        /// Use `Never` if this `Publisher` does not publish errors.
-        public typealias Failure = Upstream.Failure
-
-        public let upstream: Upstream
-
-        public let maxPublishers: Subscribers.Demand
-
-        public let transform: (Upstream.Output) -> NewPublisher
-
-        public init(upstream: Upstream, maxPublishers: Subscribers.Demand, transform: @escaping (Upstream.Output) -> NewPublisher)
-
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where S : Subscriber, NewPublisher.Output == S.Input, Upstream.Failure == S.Failure
-    }
-}
-
-extension Publisher {
-
-    /// Transforms all elements from an upstream publisher into a new or existing publisher.
-    ///
-    /// `flatMap` merges the output from all returned publishers into a single stream of output.
-    ///
-    /// - Parameters:
-    ///   - maxPublishers: The maximum number of publishers produced by this method.
-    ///   - transform: A closure that takes an element as a parameter and returns a publisher
-    /// that produces elements of that type.
-    /// - Returns: A publisher that transforms elements from an upstream publisher into
-    /// a publisher of that element’s type.
-    public func flatMap<T, P>(maxPublishers: Subscribers.Demand = .unlimited, _ transform: @escaping (Self.Output) -> P) -> Publishers.FlatMap<P, Self> where T == P.Output, P : Publisher, Self.Failure == P.Failure
-}
-
-extension Publishers {
-
     /// A publisher that delays delivery of elements and completion to the downstream receiver.
     public struct Delay<Upstream, Context> : Publisher where Upstream : Publisher, Context : Scheduler {
 
