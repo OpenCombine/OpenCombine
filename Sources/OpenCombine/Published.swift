9599754e1ec9b70065397ebaf4f3fa682070a4ae
Evgeny Bogomolov <beastgrim@gmail.com>
2019-09-01 16:36:16 +0300

    Fixed SwiftLint warnings

--- Sources/OpenCombine/Published.swift
+++ Sources/OpenCombine/Published.swift
@@ -5,16 +5,15 @@
 //  Created by Евгений Богомолов on 01/09/2019.
 //
 
-import Foundation
-
 @propertyWrapper public struct Published<Value> {
 
-    /// Initialize the storage of the Published property as well as the corresponding `Publisher`.
+    /// Initialize the storage of the Published
+    /// property as well as the corresponding `Publisher`.
     public init(initialValue: Value) {
         self.value = initialValue
     }
 
-    public struct Publisher : OpenCombine.Publisher {
+    public struct Publisher: OpenCombine.Publisher {
 
         /// The kind of values published by this publisher.
         public typealias Output = Value
@@ -24,29 +23,32 @@ import Foundation
         /// Use `Never` if this `Publisher` does not publish errors.
         public typealias Failure = Never
 
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
+        /// This function is called to attach the specified
+        /// `Subscriber` to this `Publisher` by `subscribe(_:)`
         ///
         /// - SeeAlso: `subscribe(_:)`
         /// - Parameters:
         ///     - subscriber: The subscriber to attach to this `Publisher`.
         ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where Value == S.Input, S : Subscriber, S.Failure == Published<Value>.Publisher.Failure {
-            
-            _passthrowObject.receive(subscriber: subscriber)
+        public func receive<SubscriberType>(subscriber: SubscriberType)
+            where Value == SubscriberType.Input,
+            SubscriberType: Subscriber,
+            SubscriberType.Failure == Published<Value>.Publisher.Failure
+        {
+            _passthrowObject.subscribe(subscriber)
         }
-        
-        
-        // MARK: - Private
-        private var _passthrowObject = OpenCombine.PassthroughSubject<Value,Never>()
-        
+
+        private let _passthrowObject = OpenCombine.PassthroughSubject<Value, Never>()
+
         internal func send(_ input: Output) {
             _passthrowObject.send(input)
         }
     }
 
-    /// The property that can be accessed with the `$` syntax and allows access to the `Publisher`
+    /// The property that can be accessed with the
+    /// `$` syntax and allows access to the `Publisher`
     public private(set) lazy var projectedValue: Published<Value>.Publisher = .init()
-    
+
     public var wrappedValue: Value {
         get { value }
         set {
@@ -54,11 +56,12 @@ import Foundation
             projectedValue.send(newValue)
         }
      }
-    
-    // MARK: - Private
+
     private var value: Value
-    
-    // For removing warning: Property wrapper's `init(initialValue:)` should be renamed to 'init(wrappedValue:)'; use of 'init(initialValue:)' is deprecated
+
+    /// For removing warning: Property wrapper's `init(initialValue:)`
+    /// should be renamed to 'init(wrappedValue:)';
+    /// use of 'init(initialValue:)' is deprecated
     public init(wrappedValue: Value) {
         self.value = wrappedValue
     }
