72753ef93c33153914c8e42e3b4fdff7fe7f770d
Sergej Jaskiewicz <broadwaylamb@users.noreply.github.com>
2019-09-19 14:06:57 +0300

    Implement Publishers.MakeConnectable (#61)
    * Implement Publishers.MakeConnectable
    
    * Add MakeConnectable tests

---
+++ Tests/OpenCombineTests/PublisherTests/MakeConnectableTests.swift
@@ -0,0 +1,39 @@
+//
+//  MakeConnectableTests.swift
+//  
+//
+//  Created by Sergej Jaskiewicz on 19/09/2019.
+//
+
+import XCTest
+
+#if OPENCOMBINE_COMPATIBILITY_TEST
+import Combine
+#else
+import OpenCombine
+#endif
+
+@available(macOS 10.15, iOS 13.0, *)
+final class MakeConnectableTests: XCTestCase {
+
+    // MakeConnectable is just Multicast that uses PassthroughSubject,
+    // so we can reuse our existing tests
+
+    func testMulticast() throws {
+        try MulticastTests.testGenericMulticast {
+            $0.makeConnectable()
+        }
+    }
+
+    func testMulticastConnectTwice() {
+        MulticastTests.testGenericMulticastConnectTwice {
+            $0.makeConnectable()
+        }
+    }
+
+    func testMulticastDisconnect() {
+        MulticastTests.testGenericMulticastDisconnect {
+            $0.makeConnectable()
+        }
+    }
+}
