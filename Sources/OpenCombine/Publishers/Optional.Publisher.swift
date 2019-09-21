e00a6f06fc2d39807aea6a21803399011d8df50c
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-09-21 23:13:59 +0300

    Audit Optional.Publisher for thread safety (nothing to do here)

--- Sources/OpenCombine/Publishers/Optional.Publisher.swift
+++ Sources/OpenCombine/Publishers/Optional.Publisher.swift
@@ -75,35 +75,43 @@ extension Optional {
 }
 
 extension Optional.OCombine {
-    private final class Inner<Downstream: Subscriber>: Subscription,
-                                                       CustomStringConvertible,
-                                                       CustomReflectable
+    private final class Inner<Downstream: Subscriber>
+        : Subscription,
+          CustomStringConvertible,
+          CustomReflectable,
+          CustomPlaygroundDisplayConvertible
+        where Downstream.Input == Wrapped
     {
-        private let _output: Downstream.Input
-        private var _downstream: Downstream?
+        // NOTE: this class has been audited for thread safety.
+        // Combine doesn't use any locking here.
 
-        init(value: Downstream.Input, downstream: Downstream) {
-            _output = value
-            _downstream = downstream
+        private var downstream: Downstream?
+        private let output: Wrapped
+
+        init(value: Wrapped, downstream: Downstream) {
+            self.output = value
+            self.downstream = downstream
         }
 
         func request(_ demand: Subscribers.Demand) {
-            if let downstream = _downstream, demand > 0 {
-                _ = downstream.receive(_output)
-                downstream.receive(completion: .finished)
-                _downstream = nil
-            }
+            demand.assertNonZero()
+            guard let downstream = self.downstream else { return }
+            self.downstream = nil
+            _ = downstream.receive(output)
+            downstream.receive(completion: .finished)
         }
 
         func cancel() {
-            _downstream = nil
+            downstream = nil
         }
 
         var description: String { return "Optional" }
 
         var customMirror: Mirror {
-            return Mirror(self, unlabeledChildren: CollectionOfOne(_output))
+            return Mirror(self, unlabeledChildren: CollectionOfOne(output))
         }
+
+        var playgroundDescription: Any { return description }
     }
 }
 
