f0f2c5476564af1a76c56b6dfcf8f819344520c5
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-10-09 18:25:58 +0300

    Implement Collect. Use ReduceProducer.

--- RemainingCombineInterface.swift
+++ RemainingCombineInterface.swift
@@ -559,31 +559,6 @@ extension Publishers {
         public func receive<S>(subscriber: S) where S : Subscriber, Upstream.Failure == S.Failure, S.Input == [Upstream.Output]
     }
 
-    /// A publisher that buffers items.
-    public struct Collect<Upstream> : Publisher where Upstream : Publisher {
-
-        /// The kind of values published by this publisher.
-        public typealias Output = [Upstream.Output]
-
-        /// The kind of errors this publisher might publish.
-        ///
-        /// Use `Never` if this `Publisher` does not publish errors.
-        public typealias Failure = Upstream.Failure
-
-        /// The publisher that this publisher receives elements from.
-        public let upstream: Upstream
-
-        public init(upstream: Upstream)
-
-        /// This function is called to attach the specified `Subscriber` to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
-        public func receive<S>(subscriber: S) where S : Subscriber, Upstream.Failure == S.Failure, S.Input == [Upstream.Output]
-    }
-
     /// A publisher that buffers a maximum number of items.
     public struct CollectByCount<Upstream> : Publisher where Upstream : Publisher {
 
@@ -614,15 +589,6 @@ extension Publishers {
 }
 
 extension Publisher {
-
-    /// Collects all received elements, and emits a single array of the collection when the upstream publisher finishes.
-    ///
-    /// If the upstream publisher fails with an error, this publisher forwards the error to the downstream receiver instead of sending its output.
-    /// This publisher requests an unlimited number of elements from the upstream publisher. It only sends the collected array to its downstream after a request whose demand is greater than 0 items.
-    /// Note: This publisher uses an unbounded amount of memory to store the received values.
-    ///
-    /// - Returns: A publisher that collects all received items and returns them as an array upon completion.
-    public func collect() -> Publishers.Collect<Self>
 
     /// Collects up to the specified number of elements, and then emits a single array of the collection.
     ///
