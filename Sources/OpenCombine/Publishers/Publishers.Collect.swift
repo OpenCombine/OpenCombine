bd0b69d7cb9891e8badf36a38a41e298df3de139
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-10-09 18:25:58 +0300

    Implement Collect. Use ReduceProducer.

---
+++ Sources/OpenCombine/Publishers/Publishers.Collect.swift
@@ -0,0 +1,83 @@
+//
+//  Publishers.Collect.swift
+//  
+//
+//  Created by Sergej Jaskiewicz on 09.10.2019.
+//
+
+extension Publisher {
+
+    /// Collects all received elements, and emits a single array of the collection when
+    /// the upstream publisher finishes.
+    ///
+    /// If the upstream publisher fails with an error, this publisher forwards the error
+    /// to the downstream receiver instead of sending its output.
+    /// This publisher requests an unlimited number of elements from the upstream
+    /// publisher. It only sends the collected array to its downstream after a request
+    /// whose demand is greater than 0 items.
+    /// Note: This publisher uses an unbounded amount of memory to store the received
+    /// values.
+    ///
+    /// - Returns: A publisher that collects all received items and returns them as
+    ///   an array upon completion.
+    public func collect() -> Publishers.Collect<Self> {
+        return .init(upstream: self)
+    }
+}
+
+extension Publishers {
+
+    /// A publisher that buffers items.
+    public struct Collect<Upstream: Publisher>: Publisher {
+
+        public typealias Output = [Upstream.Output]
+
+        public typealias Failure = Upstream.Failure
+
+        /// The publisher that this publisher receives elements from.
+        public let upstream: Upstream
+
+        public init(upstream: Upstream) {
+            self.upstream = upstream
+        }
+
+        public func receive<Downstream: Subscriber>(subscriber: Downstream)
+            where Upstream.Failure == Downstream.Failure,
+                  Downstream.Input == [Upstream.Output]
+        {
+            upstream.subscribe(Inner(downstream: subscriber))
+        }
+    }
+}
+
+extension Publishers.Collect {
+    private final class Inner<Downstream: Subscriber>
+        : ReduceProducer<Downstream,
+                         Upstream.Output,
+                         [Upstream.Output],
+                         Upstream.Failure,
+                         Void>
+        where Downstream.Input == [Upstream.Output],
+              Downstream.Failure == Upstream.Failure
+    {
+        fileprivate init(downstream: Downstream) {
+            super.init(downstream: downstream, initial: [], reduce: ())
+        }
+
+        override func receive(
+            newValue: Upstream.Output
+        ) -> PartialCompletion<Void, Downstream.Failure> {
+            result!.append(newValue)
+            return .continue
+        }
+
+        override var description: String {
+            return "Collect"
+        }
+
+        override var customMirror: Mirror {
+            let children: CollectionOfOne<Mirror.Child> = .init(("count", result!.count))
+            return Mirror(self, children: children)
+        }
+    }
+}
---
+++ Tests/OpenCombineTests/PublisherTests/CollectTests.swift
@@ -0,0 +1,65 @@
+//
+//  CollectTests.swift
+//  
+//
+//  Created by Sergej Jaskiewicz on 15.10.2019.
+//
+
+import XCTest
+
+#if OPENCOMBINE_COMPATIBILITY_TEST
+import Combine
+#else
+import OpenCombine
+#endif
+
+@available(macOS 10.15, iOS 13.0, *)
+final class CollectTests: XCTestCase {
+
+    func testBasicBehavior() throws {
+        try ReduceTests.testBasicReductionBehavior(expectedSubscription: "Collect",
+                                                   expectedResult: [1, 2, 3, 4, 5],
+                                                   { $0.collect() })
+    }
+
+    func testUpstreamFinishesWithError() {
+        ReduceTests.testUpstreamFinishesWithError(expectedSubscription: "Collect",
+                                                  { $0.collect() })
+    }
+
+    func testtestUpstreamFinishesImmediately() {
+        ReduceTests.testUpstreamFinishesImmediately(expectedSubscription: "Collect",
+                                                    expectedResult: [],
+                                                    { $0.collect() })
+    }
+
+    func testCancelAlreadyCancelled() throws {
+        try ReduceTests.testCancelAlreadyCancelled { $0.collect() }
+    }
+
+    func testRequestsUnlimitedThenSendsSubscription() {
+        ReduceTests.testRequestsUnlimitedThenSendsSubscription { $0.collect() }
+    }
+
+    func testReceiveSubscriptionTwice() throws {
+        try ReduceTests
+            .testReceiveSubscriptionTwice(expectedSubscription: "Collect",
+                                          expectedResult: .normalCompletion([0]),
+                                          { $0.collect() })
+    }
+
+    func testCollectLifecycle() throws {
+        try testLifecycle(sendValue: 31,
+                          cancellingSubscriptionReleasesSubscriber: false,
+                          { $0.collect() })
+    }
+
+    func testCollectReflection() throws {
+        try testReflection(parentInput: Int.self,
+                           parentFailure: Error.self,
+                           description: "Collect",
+                           customMirror: expectedChildren(("count", "0")),
+                           playgroundDescription: "Collect",
+                           { $0.collect() })
+    }
+}
