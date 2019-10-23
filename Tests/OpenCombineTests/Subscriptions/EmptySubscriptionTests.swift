1589bc6db6e353f282c51def1fdad9aaf17b41f8
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-10-23 17:19:45 +0300

    Add tests for Subscriptions.empty

---
+++ Tests/OpenCombineTests/Subscriptions/EmptySubscriptionTests.swift
@@ -0,0 +1,41 @@
+//
+//  EmptySubscriptionTests.swift
+//  
+//
+//  Created by Sergej Jaskiewicz on 23.10.2019.
+//
+
+import XCTest
+
+#if OPENCOMBINE_COMPATIBILITY_TEST
+import Combine
+#else
+import OpenCombine
+#endif
+
+@available(macOS 10.15, iOS 13.0, *)
+final class EmptySubscriptionTests: XCTestCase {
+
+    func testSingleInstance() {
+        XCTAssertEqual(Subscriptions.empty.combineIdentifier,
+                       Subscriptions.empty.combineIdentifier)
+    }
+
+    func testReflection() throws {
+        XCTAssertEqual((Subscriptions.empty as? CustomStringConvertible)?.description,
+                       "Empty")
+
+        XCTAssertEqual(
+            (Subscriptions.empty as? CustomPlaygroundDisplayConvertible)?
+                .playgroundDescription as? String,
+            "Empty"
+        )
+
+        XCTAssertFalse(Subscriptions.empty is CustomDebugStringConvertible)
+
+        let mirror =
+            try XCTUnwrap((Subscriptions.empty as? CustomReflectable)?.customMirror)
+
+        XCTAssertTrue(childrenIsEmpty(mirror))
+    }
+}
