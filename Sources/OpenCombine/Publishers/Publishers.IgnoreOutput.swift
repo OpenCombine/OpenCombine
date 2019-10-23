ef2843fe5780a46b617682ac227c21bff301deb8
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-10-23 21:09:44 +0300

    Audit IgnoreOutput for thread safety, add more tests, make them pass

--- Sources/OpenCombine/Publishers/Publishers.IgnoreOutput.swift
+++ Sources/OpenCombine/Publishers/Publishers.IgnoreOutput.swift
@@ -4,6 +4,8 @@
 //  Created by Eric Patey on 16.08.2019.
 //
 
+import COpenCombineHelpers
+
 extension Publisher {
 
     /// Ingores all upstream elements, but passes along a completion
@@ -12,7 +14,7 @@ extension Publisher {
     /// The output type of this publisher is `Never`.
     /// - Returns: A publisher that ignores all upstream elements.
     public func ignoreOutput() -> Publishers.IgnoreOutput<Self> {
-        return Publishers.IgnoreOutput(upstream: self)
+        return .init(upstream: self)
     }
 }
 
@@ -21,12 +23,8 @@ extension Publishers {
     /// state (finish or failed).
     public struct IgnoreOutput<Upstream: Publisher>: Publisher {
 
-        /// The kind of values published by this publisher.
         public typealias Output = Never
 
-        /// The kind of errors this publisher might publish.
-        ///
-        /// Use `Never` if this `Publisher` does not publish errors.
         public typealias Failure = Upstream.Failure
 
         /// The publisher from which this publisher receives elements.
@@ -36,39 +34,52 @@ extension Publishers {
             self.upstream = upstream
         }
 
-        /// This function is called to attach the specified `Subscriber`
-        /// to this `Publisher` by `subscribe(_:)`
-        ///
-        /// - SeeAlso: `subscribe(_:)`
-        /// - Parameters:
-        ///     - subscriber: The subscriber to attach to this `Publisher`.
-        ///                   once attached it can begin to receive values.
         public func receive<Downstream: Subscriber>(subscriber: Downstream)
-            where Downstream.Failure == Upstream.Failure, Downstream.Input == Never {
-            let inner = Inner<Downstream>(downstream: subscriber)
-            upstream.subscribe(inner)
+            where Downstream.Failure == Upstream.Failure, Downstream.Input == Never
+        {
+            upstream.subscribe(Inner<Downstream>(downstream: subscriber))
         }
     }
 }
 
 extension Publishers.IgnoreOutput {
-
     private final class Inner<Downstream: Subscriber>
-        : OperatorSubscription<Downstream>,
-          Subscriber,
+        : Subscriber,
+          Subscription,
           CustomStringConvertible,
-          Subscription
-    where Downstream.Input == Never,
-          Downstream.Failure == Upstream.Failure
+          CustomReflectable,
+          CustomPlaygroundDisplayConvertible
+        where Downstream.Input == Never, Downstream.Failure == Upstream.Failure
     {
+        // NOTE: This class has been audited for thread safety
+
         typealias Input = Upstream.Output
-        typealias Output = Never
+
         typealias Failure = Upstream.Failure
 
-        var description: String { return "IgnoreOutput" }
+        private let downstream: Downstream
+
+        private var status = SubscriptionStatus.awaitingSubscription
+
+        private let lock = UnfairLock.allocate()
+
+        fileprivate init(downstream: Downstream) {
+            self.downstream = downstream
+        }
+
+        deinit {
+            lock.deallocate()
+        }
 
         func receive(subscription: Subscription) {
-            upstreamSubscription = subscription
+            lock.lock()
+            guard case .awaitingSubscription = status else {
+                lock.unlock()
+                subscription.cancel()
+                return
+            }
+            status = .subscribed(subscription)
+            lock.unlock()
             downstream.receive(subscription: self)
             subscription.request(.unlimited)
         }
@@ -78,6 +89,13 @@ extension Publishers.IgnoreOutput {
         }
 
         func receive(completion: Subscribers.Completion<Failure>) {
+            lock.lock()
+            guard case .subscribed = status else {
+                lock.unlock()
+                return
+            }
+            status = .terminal
+            lock.unlock()
             downstream.receive(completion: completion)
         }
 
@@ -85,6 +103,31 @@ extension Publishers.IgnoreOutput {
             // ignore and requests from downstream since we'll never send
             // any values
         }
+
+        func cancel() {
+            lock.lock()
+            guard case let .subscribed(subscription) = status else {
+                lock.unlock()
+                return
+            }
+            status = .terminal
+            lock.unlock()
+            subscription.cancel()
+        }
+
+        var description: String { return "IgnoreOutput" }
+
+        var customMirror: Mirror {
+            lock.lock()
+            defer { lock.unlock() }
+            let children: [Mirror.Child] = [
+                ("downstream", downstream),
+                ("status", status)
+            ]
+            return Mirror(self, children: children)
+        }
+
+        var playgroundDescription: Any { return description }
     }
 }
 
