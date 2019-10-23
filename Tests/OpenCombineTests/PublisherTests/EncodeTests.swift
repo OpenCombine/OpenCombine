f5da2f90781d6f95156a97817f74b532bc0d6362
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-10-23 15:01:58 +0300

    Gybify Encode/Decode, add more tests, make them pass

--- Tests/OpenCombineTests/PublisherTests/EncodeTests.swift
+++ Tests/OpenCombineTests/PublisherTests/EncodeTests.swift
@@ -25,40 +25,48 @@ final class EncodeTests: XCTestCase {
         decoder = TestDecoder()
     }
 
+    // MARK: - Encode
+
     func testEncodingSuccess() throws {
-        let testValue = ["test" : "TestDecodable"]
-        let subject = PassthroughSubject<[String : String], Error>()
-        let publisher = subject.encode(encoder: encoder)
-        let subscriber = TrackingSubscriberBase<Int, Error>(
-            receiveSubscription: { $0.request(.unlimited) }
-        )
 
-        publisher.subscribe(subscriber)
-        subject.send(testValue)
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisherBase<[String : String], Error>.self,
+            initialDemand: .unlimited,
+            receiveValueDemand: .none,
+            createSut: { $0.encode(encoder: encoder) })
 
-        XCTAssertEqual(encoder.encoded.first?.value as? [String : String], testValue)
+        XCTAssertEqual(helper.publisher.send(["test": "test1"]), .none)
+        XCTAssertEqual(helper.publisher.send(["test": "test2"]), .none)
+
+        XCTAssertEqual(encoder.encoded[1] as? [String : String], ["test": "test1"])
+        XCTAssertEqual(encoder.encoded[2] as? [String : String], ["test": "test2"])
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("Encode"),
+                                                 .value(1),
+                                                 .value(2)])
+
+        helper.publisher.send(completion: .finished)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("Encode"),
+                                                 .value(1),
+                                                 .value(2),
+                                                 .completion(.finished)])
+
+        XCTAssertEqual(helper.publisher.send(testValue), .none)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("Encode"),
+                                                 .value(1),
+                                                 .value(2),
+                                                 .completion(.finished)])
     }
 
     func testEncodingFailure() throws {
-        let testValue = ["test" : "TestDecodable"]
-        let subject = PassthroughSubject<[String : String], Error>()
-
         encoder.handleEncode = { _ in throw TestingError.oops }
-
-        let publisher = subject.encode(encoder: encoder)
-        let subscriber = TrackingSubscriberBase<Int, Error>(
-            receiveSubscription: { $0.request(.unlimited) }
-        )
-
-        publisher.subscribe(subscriber)
-        subject.send(testValue)
-
-        XCTAssertEqual(subscriber.history, [.subscription("Encode"),
-                                            .completion(.failure(TestingError.oops))])
+        try EncodeTests.testCodingFailure(expectedSubscription: "Encode",
+                                          { $0.encode(encoder: encoder) })
     }
 
     func testEncodeSuccessHistory() throws {
-        let testValue = ["test" : "TestDecodable"]
         let subject = PassthroughSubject<[String : String], Error>()
         let publisher = subject.encode(encoder: encoder)
         let subscriber = TrackingSubscriberBase<Int, Error>(
@@ -76,29 +84,326 @@ final class EncodeTests: XCTestCase {
                                             .value(testKey)])
     }
 
-    func testDemand() {
-        let subscription = CustomSubscription()
+    func testEncodeDemand() throws {
+        try EncodeTests.testDemand {
+            $0.encode(encoder: encoder)
+        }
+    }
 
-        let publisher = CustomPublisherBase<[String : String], TestingError>(
-            subscription: subscription
+    func testEncodeReceiveSubscriptionTwice() throws {
+        try EncodeTests.testReceiveSubscriptionTwice { $0.encode(encoder: encoder) }
+    }
+
+    func testEncodeCancelsSubscriptionThenReleasesIt() throws {
+        try EncodeTests.testCancelsSubscriptionThenReleasesIt(
+            expectedSubscription: "Encode",
+            { $0.encode(encoder: encoder) }
+        )
+    }
+
+    func testEncodeCompletionReleasesUpstreamSubscription() throws {
+        try EncodeTests.testCompletionReleasesUpstreamSubscription(
+            expectedSubscription: "Encode",
+            { $0.encode(encoder: encoder) }
+        )
+    }
+
+    func testEncodeCancellingSubscriptionPreventsDeliveringToDownstream() throws {
+        try EncodeTests.testCancellingSubscriptionPreventsDeliveringToDownstream(
+            expectedSubscription: "Encode",
+            { $0.encode(encoder: TestEncoder()) }
+        )
+    }
+
+    func testEncodeLifecycle() throws {
+        try testLifecycle(sendValue: 31,
+                          cancellingSubscriptionReleasesSubscriber: false,
+                          { $0.encode(encoder: TestEncoder()) })
+    }
+
+    func testEncodeReflection() throws {
+        try testReflection(parentInput: Int.self,
+                           parentFailure: Error.self,
+                           description: "Encode",
+                           customMirror: expectedChildren(
+                               ("downstream", .contains("TrackingSubscriberBase")),
+                               ("finished", "false"),
+                               ("upstreamSubscription", "nil")
+                           ),
+                           playgroundDescription: "Encode",
+                           { $0.encode(encoder: encoder) })
+    }
+
+    // MARK: - Decode
+
+    func testDecodingSuccess() throws {
+        decoder.handleDecode = { ["key\($0)" : "value\($0)"] }
+
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: .unlimited,
+            receiveValueDemand: .none,
+            createSut: { $0.decode(type: [String : String].self, decoder: decoder) })
+
+        XCTAssertEqual(helper.publisher.send(1), .none)
+        XCTAssertEqual(helper.publisher.send(2), .none)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("Decode"),
+                                                 .value(["key1" : "value1"]),
+                                                 .value(["key2" : "value2"])])
+
+        helper.publisher.send(completion: .finished)
+
+        XCTAssertEqual(helper.publisher.send(3), .none)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription("Decode"),
+                                                 .value(["key1" : "value1"]),
+                                                 .value(["key2" : "value2"]),
+                                                 .completion(.finished)])
+    }
+
+    func testDecodingFailure() throws {
+        decoder.handleDecode = { _ in throw TestingError.oops }
+        try EncodeTests.testCodingFailure(
+            expectedSubscription: "Decode",
+            { $0.decode(type: [String : String].self, decoder: decoder) }
+        )
+    }
+
+    func testDecodeDemand() throws {
+        decoder.handleDecode = { _ in testValue }
+        try EncodeTests.testDemand {
+            $0.decode(type: [String : String].self, decoder: decoder)
+        }
+    }
+
+    func testDecodeReceiveSubscriptionTwice() throws {
+        try EncodeTests.testReceiveSubscriptionTwice {
+            $0.decode(type: [String : String].self, decoder: decoder)
+        }
+    }
+
+    func testDecodeCancelsSubscriptionThenReleasesIt() throws {
+        decoder.handleDecode = { _ in testValue }
+        try EncodeTests.testCancelsSubscriptionThenReleasesIt(
+            expectedSubscription: "Decode",
+            { $0.decode(type: [String : String].self, decoder: decoder) }
+        )
+    }
+
+    func testDecodeCompletionReleasesUpstreamSubscription() throws {
+        decoder.handleDecode = { _ in testValue }
+        try EncodeTests.testCompletionReleasesUpstreamSubscription(
+            expectedSubscription: "Decode",
+            { $0.decode(type: [String : String].self, decoder: decoder) }
+        )
+    }
+
+    func testDecodeCancellingSubscriptionPreventsDeliveringToDownstream() throws {
+        decoder.handleDecode = { _ in testValue }
+        try EncodeTests.testCancellingSubscriptionPreventsDeliveringToDownstream(
+            expectedSubscription: "Decode",
+            { $0.decode(type: [String : String].self, decoder: decoder) }
+        )
+    }
+
+    func testDecodeLifecycle() throws {
+        try testLifecycle(sendValue: 31,
+                          cancellingSubscriptionReleasesSubscriber: false,
+                          { $0.decode(type: Int.self, decoder: decoder) })
+    }
+
+    func testDecodeReflection() throws {
+        try testReflection(parentInput: Int.self,
+                           parentFailure: Error.self,
+                           description: "Decode",
+                           customMirror: expectedChildren(
+                               ("downstream", .contains("TrackingSubscriberBase")),
+                               ("finished", "false"),
+                               ("upstreamSubscription", "nil")
+                           ),
+                           playgroundDescription: "Decode",
+                           { $0.decode(type: Int.self, decoder: decoder) })
+    }
+
+    // MARK: - Generic tests
+
+    private static func testCodingFailure<Operator: Publisher>(
+        expectedSubscription: StringSubscription,
+        _ makeOperator: (CustomPublisher) -> Operator
+    ) throws where Operator.Output: Equatable, Operator.Failure == Error {
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: .unlimited,
+            receiveValueDemand: .none,
+            createSut: makeOperator
         )
 
-        let encode = publisher.encode(encoder: encoder)
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited)])
 
-        var downstreamSubscription: Subscription?
+        helper.subscription.onCancel = {
+            helper.downstreamSubscription?.request(.max(42))
 
-        let tracking = TrackingSubscriberBase<Int, Error>(
-            receiveSubscription: {
-                $0.request(.max(37))
-                downstreamSubscription = $0
-            },
-            receiveValue: { _ in .max(2) }
+            XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+            XCTAssertEqual(helper.subscription.history, [.requested(.unlimited),
+                                                         .cancelled,
+                                                         .requested(.max(42))])
+        }
+
+        helper.tracking.onFailure = { error in
+            XCTAssertEqual(helper.tracking.history,
+                           [.subscription(expectedSubscription),
+                            .completion(.failure(error))])
+            XCTAssertEqual(helper.subscription.history, [.requested(.unlimited),
+                                                         .cancelled,
+                                                         .requested(.max(42))])
+        }
+
+        XCTAssertEqual(helper.publisher.send(1), .none)
+        XCTAssertEqual(helper.publisher.send(2), .none)
+
+        XCTAssertEqual(helper.tracking.history,
+                       [.subscription(expectedSubscription),
+                        .completion(.failure(TestingError.oops))])
+        XCTAssertEqual(helper.subscription.history, [.requested(.unlimited),
+                                                     .cancelled,
+                                                     .requested(.max(42))])
+    }
+
+    private static func testDemand<Operator: Publisher>(
+        _ makeOperator: (CustomPublisherBase<Int, Error>) -> Operator
+    ) throws {
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisherBase<Int, Error>.self,
+            initialDemand: .max(37),
+            receiveValueDemand: .max(2),
+            createSut: makeOperator
         )
 
-        encode.subscribe(tracking)
-        XCTAssertNotNil(downstreamSubscription)
+        XCTAssertEqual(helper.publisher.send(37), .max(2))
+        XCTAssertEqual(helper.subscription.history, [.requested(.max(37))])
 
-        XCTAssertEqual(publisher.send(["test" : "TestDecodable"]), .max(2))
-        XCTAssertEqual(subscription.history, [.requested(.max(37))])
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+
+        XCTAssertEqual(helper.publisher.send(42), .none)
+        XCTAssertEqual(helper.subscription.history, [.requested(.max(37)), .cancelled])
+    }
+
+    private static func testReceiveSubscriptionTwice<Operator: Publisher>(
+        _ makeOperator: (CustomPublisher) -> Operator
+    ) throws where Operator.Output: Equatable {
+        let helper = OperatorTestHelper(
+            publisherType: CustomPublisher.self,
+            initialDemand: nil,
+            receiveValueDemand: .none,
+            createSut: makeOperator
+        )
+
+        XCTAssertEqual(helper.subscription.history, [])
+
+        let secondSubscription = CustomSubscription()
+
+        try XCTUnwrap(helper.publisher.subscriber)
+            .receive(subscription: secondSubscription)
+
+        XCTAssertEqual(secondSubscription.history, [.cancelled])
+
+        try XCTUnwrap(helper.publisher.subscriber)
+            .receive(subscription: helper.subscription)
+
+        XCTAssertEqual(helper.subscription.history, [.cancelled])
+
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+
+        XCTAssertEqual(helper.subscription.history, [.cancelled, .cancelled])
+
+        let thirdSubscription = CustomSubscription()
+
+        try XCTUnwrap(helper.publisher.subscriber)
+            .receive(subscription: thirdSubscription)
+
+        XCTAssertEqual(thirdSubscription.history, [.cancelled])
+    }
+
+    private static func testCancelsSubscriptionThenReleasesIt<Operator: Publisher>(
+        expectedSubscription: StringSubscription,
+        _ makeOperator: (CustomPublisher) -> Operator
+    ) throws where Operator.Output: Equatable {
+        let helper = OperatorTestHelper(publisherType: CustomPublisher.self,
+                                        initialDemand: nil,
+                                        receiveValueDemand: .none,
+                                        createSut: makeOperator)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+        XCTAssertEqual(helper.subscription.history, [])
+
+        var recursionDepth = 0
+        helper.subscription.onCancel = {
+            if recursionDepth >= 5 { return }
+            recursionDepth += 1
+            helper.downstreamSubscription?.cancel()
+        }
+
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+
+        XCTAssertEqual(recursionDepth, 5)
+    }
+
+    private static func testCancellingSubscriptionPreventsDeliveringToDownstream<
+        Operator: Publisher
+    >(
+        expectedSubscription: StringSubscription,
+        _ makeOperator: (CustomPublisher) -> Operator
+    ) throws where Operator.Output: Equatable {
+        let helper = OperatorTestHelper(publisherType: CustomPublisher.self,
+                                        initialDemand: nil,
+                                        receiveValueDemand: .none,
+                                        createSut: makeOperator)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+        XCTAssertEqual(helper.subscription.history, [])
+
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+        try XCTUnwrap(helper.downstreamSubscription).cancel()
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+        XCTAssertEqual(helper.subscription.history, [.cancelled])
+
+        XCTAssertEqual(helper.publisher.send(42), .none)
+        helper.publisher.send(completion: .finished)
+        helper.publisher.send(completion: .finished)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+        XCTAssertEqual(helper.subscription.history, [.cancelled])
+    }
+
+    private static func testCompletionReleasesUpstreamSubscription<Operator: Publisher>(
+        expectedSubscription: StringSubscription,
+        _ makeOperator: (CustomPublisher) -> Operator
+    ) throws where Operator.Output: Equatable {
+        let helper = OperatorTestHelper(publisherType: CustomPublisher.self,
+                                        initialDemand: nil,
+                                        receiveValueDemand: .none,
+                                        createSut: makeOperator)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription)])
+        XCTAssertEqual(helper.subscription.history, [])
+
+        helper.publisher.send(completion: .finished)
+        helper.publisher.send(completion: .finished)
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription),
+                                                 .completion(.finished)])
+        XCTAssertEqual(helper.subscription.history, [])
+
+        try XCTUnwrap(helper.downstreamSubscription).request(.max(1))
+        try XCTUnwrap(helper.downstreamSubscription).request(.max(1))
+
+        XCTAssertEqual(helper.tracking.history, [.subscription(expectedSubscription),
+                                                 .completion(.finished)])
+        XCTAssertEqual(helper.subscription.history, [])
     }
 }
+
+private let testValue = ["test": "TestDecodable"]
