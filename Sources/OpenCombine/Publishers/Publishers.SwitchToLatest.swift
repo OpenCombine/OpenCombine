fb184ceebefea0347413dc4eebba0fd29f1b3a73
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2020-10-02 02:45:22 +0300

    [Xcode 12] Add new switchToLatest() overloads

--- Sources/OpenCombine/Publishers/Publishers.SwitchToLatest.swift
+++ Sources/OpenCombine/Publishers/Publishers.SwitchToLatest.swift
@@ -69,6 +69,65 @@ extension Publisher where Output: Publisher, Output.Failure == Failure {
     }
 }
 
+extension Publisher where Output: Publisher, Failure == Never {
+
+    /// Republishes elements sent by the most recently received publisher.
+    ///
+    /// This operator works with an upstream publisher of publishers, flattening
+    /// the stream of elements to appear as if they were coming from a single stream of
+    /// elements. It switches the inner publisher as new ones arrive but keeps the outer
+    /// publisher constant for downstream subscribers.
+    ///
+    /// When this operator receives a new publisher from the upstream publisher, it
+    /// cancels its previous subscription. Use this feature to prevent earlier publishers
+    /// from performing unnecessary work, such as creating network request publishers from
+    /// frequently updating user interface publishers.
+    public func switchToLatest() -> Publishers.SwitchToLatest<
+        Output, Publishers.SetFailureType<Self, Output.Failure>
+    > {
+        return setFailureType(to: Output.Failure.self).switchToLatest()
+    }
+}
+
+extension Publisher where Output: Publisher, Failure == Never, Output.Failure == Never {
+
+    /// Republishes elements sent by the most recently received publisher.
+    ///
+    /// This operator works with an upstream publisher of publishers, flattening
+    /// the stream of elements to appear as if they were coming from a single stream of
+    /// elements. It switches the inner publisher as new ones arrive but keeps the outer
+    /// publisher constant for downstream subscribers.
+    ///
+    /// When this operator receives a new publisher from the upstream publisher, it
+    /// cancels its previous subscription. Use this feature to prevent earlier publishers
+    /// from performing unnecessary work, such as creating network request publishers from
+    /// frequently updating user interface publishers.
+    public func switchToLatest() -> Publishers.SwitchToLatest<Output, Self> {
+        return .init(upstream: self)
+    }
+}
+
+extension Publisher where Output: Publisher, Output.Failure == Never {
+
+    /// Republishes elements sent by the most recently received publisher.
+    ///
+    /// This operator works with an upstream publisher of publishers, flattening
+    /// the stream of elements to appear as if they were coming from a single stream of
+    /// elements. It switches the inner publisher as new ones arrive but keeps the outer
+    /// publisher constant for downstream subscribers.
+    ///
+    /// When this operator receives a new publisher from the upstream publisher, it
+    /// cancels its previous subscription. Use this feature to prevent earlier publishers
+    /// from performing unnecessary work, such as creating network request publishers from
+    /// frequently updating user interface publishers.
+    public func switchToLatest() -> Publishers.SwitchToLatest<
+        Publishers.SetFailureType<Output, Failure>,
+        Publishers.Map<Self, Publishers.SetFailureType<Output, Failure>>
+    > {
+        return map { $0.setFailureType(to: Failure.self) }.switchToLatest()
+    }
+}
+
 extension Publishers {
 
     /// A publisher that flattens nested publishers.
